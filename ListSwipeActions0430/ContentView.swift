b23d802e420f8a985258c4ade3ff41556f2bbd1b
张亚飞 <672233580@qq.com>
2022-04-30 20:58:13 +0800

    Initial Commit

---
+++ ListSwipeActions0430/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ListSwipeActions0430
+//
+//  Created by 亚飞 on 2022/4/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ListSwipeActions0430/ListSwipeActions0430App.swift
@@ -0,0 +1,17 @@
+//
+//  ListSwipeActions0430App.swift
+//  ListSwipeActions0430
+//
+//  Created by 亚飞 on 2022/4/30.
+//
+
+import SwiftUI
+
+@main
+struct ListSwipeActions0430App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
